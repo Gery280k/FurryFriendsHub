457708f1864f823e48cb2aec14855bf6fbf1cbd4
Gery280k <166630402+Gery280k@users.noreply.github.com>
2024-10-18 11:28:45 -0600

    Add files via upload

---
+++ FurryFriendsHub-BlazorAssembly.Shared/UserAuthDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FurryFriendsHub_BlazorAssembly.Shared
+{
+	public class UserAuthDTO
+	{
+		public string Email { get; set; }
+		public string passwordHash { get; set; }
+	}
+}
