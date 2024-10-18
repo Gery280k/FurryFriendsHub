da5115aca603ff7aee85ce8e03117403f6010e18
Gery280k <166630402+Gery280k@users.noreply.github.com>
2024-10-18 11:27:31 -0600

    Add files via upload

---
+++ FurryFriendsHub-BlazorAssembly/Repositories/IUserCollection.cs
@@ -0,0 +1,13 @@
+﻿using FurryFriendsHub_BlazorAssembly.Shared;
+namespace FurryFriendsHub_BlazorAssembly.Repositories
+{
+    public interface IUserCollection
+    {
+        Task InsertUser(User user);
+        Task UpdateUser(User user);
+        Task DeleteUser(string id);
+        Task<List<User>> GetAllUsers();
+        Task<User> GetUserById(string id);
+        Task<User> AuthenticateUser(UserAuthDTO userAuthDTO);
+    }
+}
---
+++ FurryFriendsHub-BlazorAssembly/Repositories/MongoDBRepository.cs
@@ -0,0 +1,17 @@
+﻿
+using MongoDB.Driver;
+
+namespace FurryFriendsHub_BlazorAssembly.Repositories
+{
+    public class MongoDBRepository
+    {
+        public MongoClient Client = new MongoClient();
+        public IMongoDatabase db;
+
+        public MongoDBRepository() 
+        { 
+            Client = new MongoClient("mongodb://localhost:27017/");
+            db = Client.GetDatabase("FurryFriendsHub");
+        }
+    }
+}
